37680910114a19da3b397af33f90a6a55c5842b1
ameen azher <ameen.azher@icloud.com>
2026-04-16 04:56:11 +0300

    Refactor edit_post route and add delete_post route

--- main.py
+++ main.py
@@ -227,8 +227,8 @@ def add_new_post():
 
 
 # Use a decorator so only an admin user can edit a post
-@admin_only
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+@admin_only
 def edit_post(post_id):
     post = db.get_or_404(BlogPost, post_id)
     edit_form = CreatePostForm(
@@ -250,13 +250,16 @@ def edit_post(post_id):
 
 
 # Use a decorator so only an admin user can delete a post
-@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+@app.route("/delete-post/<int:post_id>")
 @admin_only
-def edit_post(post_id):
+def delete_post(post_id):
     post_to_delete = db.get_or_404(BlogPost, post_id)
     db.session.delete(post_to_delete)
     db.session.commit()
     return redirect(url_for('get_all_posts'))
+
+
+
 
 
 @app.route("/about")
